3e58ebd46e6995bcbe2eb4e8d9048f69266073eb
Your Name01~JAgo2000 <e11715751@student.tuwien.ac.at>
2023-11-14 13:30:11 +0100

    testing

--- repl_test.go
+++ repl_test.go
@@ -1,7 +1,34 @@
 package main
 
-//import(
-//	"testing"
-//)
+import (
+	"testing"
+)
 
-//
+func test_clean_scantext(t *testing.T) {
+	cases := []struct {
+		input    string
+		expectet []string
+	}{
+		{
+			input: "Hello World",
+			expectet: []string{
+				"hello",
+				"world",
+			},
+		},
+	}
+	for _, cs := range cases {
+		actual := clean_scantext(cs.input)
+		if len(actual) != len(cs.expectet) {
+			t.Errorf("The lenghts are not equal %v vs %v", len(actual), len(cs.expectet))
+			continue
+		}
+		for i := range actual {
+			actualword := actual[i]
+			expectetword := cs.expectet[i]
+			if actualword != expectetword {
+				t.Errorf("The words are not equal %v vs %v", actualword, expectetword)
+			}
+		}
+	}
+}
