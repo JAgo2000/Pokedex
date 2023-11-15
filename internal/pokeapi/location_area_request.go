6a2bc1bf753d159c80a47fc315ac77a97e3a9c1d
Your Name01~JAgo2000 <e11715751@student.tuwien.ac.at>
2023-11-15 20:18:31 +0100

    testing  pokeapi

---
+++ internal/pokeapi/location_area_request.go
@@ -0,0 +1,39 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func (c *Client) ListLocationAreas() (LocationAreasResp, error) {
+	endpoint := "/location-area"
+	fullURL := baseURL + endpoint
+
+	req, err := http.NewRequest("GET", fullURL, nil) //rep=>repuest
+	if err != nil {
+		return LocationAreasResp{}, err //LocationAreasResp{} gives back a emty LocationAreasResp
+	}
+	resp, err := c.httpClient.Do(req) //resp=>reponse
+	if err != nil {
+		return LocationAreasResp{}, err //LocationAreasResp{} gives back a emty LocationAreasResp
+	}
+	defer resp.Body.Close() //closes the respObject when function closes
+
+	if resp.StatusCode > 399 {
+		return LocationAreasResp{}, fmt.Errorf("Bad Statuscode: %v", resp.StatusCode)
+	}
+
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return LocationAreasResp{}, err //LocationAreasResp{} gives back a emty LocationAreasResp
+	}
+
+	locationAreasResp := LocationAreasResp{}
+	err = json.Unmarshal(dat, &locationAreasResp)
+	if err != nil {
+		return LocationAreasResp{}, err //LocationAreasResp{} gives back a emty LocationAreasResp
+	}
+	return locationAreasResp, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/JAgo2000/Pokedex/internal/pokeapi"
+)
+
 func main() {
-	ReplLoop()
+	//ReplLoop() testing api
+	pokeapiClient := pokeapi.NewClient()
+	resp, err := pokeapiClient.ListLocationAreas()
+	if err != nil {
+		log.Fatal(err) //exits the program os.Exit(1)
+	}
+	fmt.Println(resp)
+
 }
